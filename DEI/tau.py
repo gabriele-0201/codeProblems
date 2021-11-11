64642f9d86531e54f09ba9bcf83aa445aab05f5b
Gabriele <gabriele.miotti2@gmail.com>
2021-11-12 00:49:16 +0100

    Tau not work

--- DEI/tau.py
+++ DEI/tau.py
@@ -1,3 +1,4 @@
+vars = {}
 
 class op:
 
@@ -6,28 +7,40 @@ class op:
 
     def calcValue(self):
         if(self.type == "value"):
-            print(self.value1)
+            return vars[self.value1]
         elif(self.type == "not"):
-            print("not " + self.value1)
-
-        print(self.type)
-
-        print(self.value1.calcValue())
-        print(self.value2.calcValue())
-        pass
+            return not self.value1.calcValue()
+        elif self.type == "and":
+            return self.value1.calcValue() and self.value2.calcValue()
+        elif self.type == "or":
+            return self.value1.calcValue() or self.value2.calcValue()
+        elif self.type == "implies":
+            return not self.value1.calcValue() or self.value2.calcValue()
+        elif self.type == "equals":
+            return self.value1.calcValue() == self.value2.calcValue()
 
 def getType(c):
     if c == "K":
         return "and"
     elif c == "A":
-        return "not"
+        return "or"
     elif c == "C":
         return "implies"
     elif c == "E":
         return "equal"
+    elif c == "N":
+        return "not"
+
+# TRUE
+# TRUE OR p
+# p OR NOT p
+# NOT (p AND NOT p)
+# p == p
+# (p OR q) == p OR (NOT p AND q)
+# A E Apq p K Npq
+# (p == q) -> (p -> q)
 
 # Pay attenction to this recurtion, I really need TWO findParam? study the remaining string
-
 def findParam(s):
 
     operand1 = op()
@@ -38,17 +51,19 @@ def findParam(s):
         operand1.value1 = s[0]
         s = s[1::]
         remainingString = s
-    elif s[0] == "N":
-        operand1.type = "not"
-        operand1.value1 = s[1]
-        s = s[2::]
-        remainingString = s
+        #print("variable created "+ str(operand1.value1))
+    #elif s[0] == "N":
+    #    operand1.type = "not"
+    #    operand1.value1 = s[1]
+    #    s = s[2::]
+    #    remainingString = s
+    #    #print("NOT " + str(operand1.value1))
     else:
         operand1.type = getType(s[0])
         s = s[1::]
         operand1.value1, remainingString = findParam(s)
-        operand1.value2= findParam(remainingString)[0]
-        remainingString = ""
+        if(operand1.type != "not"):
+            operand1.value2, remainingString= findParam(remainingString)
 
     return operand1, remainingString
 
@@ -61,26 +76,50 @@ while(True):
         break
     vals.append(s)
 
-# ALLORA, faccio un ciclo che parte e mi controlla la prima lettera maiuscola
-# Poi trovata la prima lettere maiuscola la rimuove dalla stringa e va alla ricerca dei parametre
-# Adesso per il primo parametro o trova una lettera minuscola (finito)
-# Oppure una maiuscola, (se N allora prendo quella dopo ed ho fatto)
-# Se Invece e' una maiuscola ricomincio in loop dalla ricerca del primo parametro
-# appena ho finito con il primo parametro inizio con il secondo che ripete le stesse cose
-
 for val in vals:
 
     taut = op()
 
     c = val[0]
-    val = val[1::]
-    if c == "p" or c == "q" or c == "r" or c == "s" or c == "t" or c == "N":
+    if c == "p" or c == "q" or c == "r" or c == "s" or c == "t":
+        taut.type = "value"
+        taut.value1 = c
+    else:
+        val = val[1::]
+        taut.type = getType(c)
+        taut.value1, remainingString = findParam(val)
+        
+        #if(taut.value1.type == "value"):
+        #   taut.value1 = taut.value1.value1
+
+        if(taut.type != "not"):
+            taut.value2 = findParam(remainingString)[0]
+
+    for c in val:
+        if c == "p" or c == "q" or c == "r" or c == "s" or c == "t":
+            vars[c] = False
+
+    prev = False
+    tautology = True
+    for i in range(pow(2,len(vars))):
+        st = format(i, "b")
+        for j in range(len(vars) - len(st)): # pass to int really usefull
+            st = '0' + st
+        input = [int(x) for x in st]
+
+        for j, k in enumerate(vars.keys()):
+            vars[k] = True if input[j] == 1 else False
+        #print(vars) 
+        out = taut.calcValue()
+        #print(out)
+        if(i == 0):
+            prev = out
+        else:
+            if prev != out:
+                tautology = False
+                break
+
+    if tautology: 
+        print("tautology")
+    else:
         print("not")
-        break
-
-    taut.type = getType(c)
-
-    taut.value1, remainingString = findParam(c)
-    taut.value1 = findParam(remainingString)[0]
-
-    print(taut.calcValue())
