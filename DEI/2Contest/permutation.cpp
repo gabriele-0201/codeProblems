cb4e413ae23f9dda11288fd0597440acefd41361
gabriele-0201 <gabriele.miotti2@gmail.com>
2021-11-19 22:23:48 +0100

    Try fix permutation

--- DEI/2Contest/permutation.cpp
+++ DEI/2Contest/permutation.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<sstream>
 #include<unordered_map>
 
 using namespace std;
@@ -9,20 +10,48 @@ unordered_map<int, int> cp2;
 
 vector<string> colors;
 
-vector<bool> chosen (n);
+vector<bool> chosen;
 vector<int> permutation;
 
+int ctn;
+int maxValue;
 string maxPaint;
 
+vector<int> ctns;
+vector<string> maxPaints;
+
+int findInColors(string s);
+
+int getVal() {
+	int sum = 0;
+
+	for(int p : permutation) {
+		sum += findInColors(colors[p]);
+	}
+
+	return sum;
+}
+
 void search() {
-	if(permutation.size() == n) {
-		for(int i = 0; i < n; i++){
-			cout << colors[permutation[i]] <<" ";
+	if(permutation.size() == colors.size()) {
+		//for(int i = 0; i < colors.size(); i++){
+		//	cout << colors[permutation[i]] <<" ";
+		//}
+		//cout <<endl;
+		ctn++;
+
+		int val = getVal();
+
+		if(val > maxValue) {
+			maxValue = val;
+			maxPaint = "";
+			for(int p : permutation) {
+				maxPaint += colors[p] + " ";
+			}
 		}
-		cout <<endl;
 	}
 	else {
-		for(int i = 0; i < n; i++){
+		for(int i = 0; i < colors.size(); i++){
 
 			//for(int j = 0; j < permutation.size(); j++){
 				//cout << permutation[j] <<" ";
@@ -47,51 +76,66 @@ void search() {
 	}
 }
 
-vector<string> separate(string s) {
-	vector<string> colors = "";
-	string tmp = "";
-	for(auto c : s) {
-		if(c == ' '){
-			colors.push_back(tmp);
-			tmp = "";
-		}
-		else
-			tmp += c;	
-	}
-	return colors;
+
+
+int findInColors(string s) {
+	for(int i = 0; i < colors.size(); i++)
+		if (colors[i] == s)
+			return i;
+	return -1;
 }
 
 int main(void) {
 
 	int t, n, m; 
+	string stringColors, color;
 	cin >> t;
 
 	for(int i = 0; i < t; i++) {
+		cp1.clear();
+		cp2.clear();
 		cin >> n;
+		chosen = vector<bool> (n);
+		ctn = 0;
+		maxPaint = "";
+		maxValue = 0;
+		colors.clear();
 
-		string c;
-		cin >> c;
-		colors = separate(c);
+		//used bevause cin do not get rid of all new line
+		cin.ignore();
+		getline(cin, stringColors, '\n');
+		stringstream X(stringColors);
+
+		while(getline(X, color, ' '))
+			colors.push_back(color);
 
 		cin >> m;
+		cin.ignore();
 
-		for(int j=0; j < ms[i]; j++) {
-			string s;
-			cin >> s;
-				
-			vector<string> couple = separate(s);
-			vector<int> coupleInt(2);
-			coupleInt[0] =	distance(colors.begin(), colors.find(colors.begin(), colors.end(), couple[0]));
-			coupleInt[1] =	distance(colors.begin(), colors.find(colors.begin(), colors.end(), couple[1]));
+		for(int j=0; j < m; j++) {
+			getline(cin, stringColors, '\n');
+			stringstream X(stringColors);
 
+			vector<string> couple;
+
+			while(getline(X, color, ' '))
+				couple.push_back(color);
 			
-			
+			cp1[findInColors(couple[0])] = findInColors(couple[1]);
+			cp2[findInColors(couple[1])] = findInColors(couple[0]);
 		}
+
+		search();
+
+		ctns.push_back(ctn);
+		maxPaints.push_back(maxPaint);
+
 	}
 
+	for(int i = 0; i < t; i++) {
+		cout << ctns[i] << endl;
+		cout << maxPaints[i] << endl;
+	}
 
-
-	cp1[1] = 2;
-	cp2[2] = 1;
-	search();
+	return 0;
 }
